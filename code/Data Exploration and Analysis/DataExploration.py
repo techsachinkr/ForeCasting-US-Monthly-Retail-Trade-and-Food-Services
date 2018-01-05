b747c54b0fb904f9943d310a20ceb6d4aa17605c
Sachin Kumar <sachinkumar.ait@live.com>
2018-01-04 20:02:09 -0500

    new plot and code changes

--- code/Data Exploration and Analysis/DataExploration.py
+++ code/Data Exploration and Analysis/DataExploration.py
@@ -87,24 +87,4 @@ ts_log_decompose = residual
 ts_log_decompose.dropna(inplace=True)
 test_stationarity(ts_log_decompose)
 
-#ACF and PACF plots:
-from statsmodels.tsa.stattools import acf, pacf
-lag_acf = acf(ts_log_diff, nlags=20)
-lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
 
-#Plot ACF: 
-plt.subplot(121) 
-plt.plot(lag_acf)
-plt.axhline(y=0,linestyle='--',color='gray')
-plt.axhline(y=-1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
-plt.axhline(y=1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
-plt.title('Autocorrelation Function')
-
-#Plot PACF:
-plt.subplot(122)
-plt.plot(lag_pacf)
-plt.axhline(y=0,linestyle='--',color='gray')
-plt.axhline(y=-1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
-plt.axhline(y=1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
-plt.title('Partial Autocorrelation Function')
-plt.tight_layout()
--- code/Modelling Data/DataModelling.py
+++ code/Modelling Data/DataModelling.py
@@ -1,3 +1,29 @@
+"""
+@author: SACHIN
+"""
+#ACF and PACF plots:
+from statsmodels.tsa.stattools import acf, pacf
+lag_acf = acf(ts_log_diff, nlags=80)
+lag_pacf = pacf(ts_log_diff, nlags=80, method='ols')
+#from pandas.tools.plotting import autocorrelation_plot
+#autocorrelation_plot(ts)
+#Plot ACF: 
+plt.subplot(121) 
+plt.plot(lag_acf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Autocorrelation Function')
+
+#Plot PACF:
+plt.subplot(122)
+plt.plot(lag_pacf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/npy.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Partial Autocorrelation Function')
+plt.tight_layout()
+
 from statsmodels.tsa.arima_model import ARIMA
 
 from sklearn.metrics import mean_squared_error
@@ -7,7 +33,7 @@ train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 predictions = list()
 for t in range(len(test)):
-	model = ARIMA(history, order=(5,1,0))
+	model = ARIMA(history, order=(10,1,0))
 	model_fit = model.fit(disp=0)
 	output = model_fit.forecast()
 	yhat = output[0]
@@ -18,6 +44,8 @@ for t in range(len(test)):
 error = mean_squared_error(test, predictions)
 print('Test MSE: %.3f' % error)
 # plot
-plt.plot(test)
-plt.plot(predictions, color='red')
+plt.plot(test,label='Actual Value')
+plt.plot(predictions, color='red', label='Predictions')
+plt.legend(loc='best')
+plt.title('AR Model')
 plt.show()
